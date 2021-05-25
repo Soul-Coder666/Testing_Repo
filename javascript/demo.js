8b47bb98f426c232a143a0951663854b77b1b083
Soul-Coder666 <johnwick6721@gmail.com>
2021-05-25 10:15:13 +0530

    This is test

---
+++ javascript/demo.js
@@ -0,0 +1,61 @@
+/*This is
+A Multiline comment*/
+//This is a single line comment
+document.write("<h3>Hello  World!!<h3>");
+console.log("This is a Console Message!!");
+alert("This is ALERT!!");
+var x = 30;
+var y = 20;
+var name = "Anurag";
+var sentence = 'My name is "Anurag"';
+var isActive = true;
+var result = 20;
+// result += y *= x;
+var string1 = "Hello";
+var string2 = "How are you";
+document.writeln(string1+string2);
+if(x > y){
+    alert("x is greater than y");
+}
+else{
+    alert("x is less than y");
+}
+var day = 2;
+switch(day){
+    
+    case 1:
+        document.write("Sunday");
+        break;
+    case 2:
+        document.write("Monday");
+        break;
+    case 3:
+        document.write("Tuesday");
+        break;
+    case 4:
+        document.write("Wednesday");
+        break;
+        default:
+            document.write("Another Day");
+}
+for(i = 1, text=""; i <= 5; i++){
+    text = i;
+    document.write(i+"<br/>");
+}
+var i=1;
+while(i <= 10){
+    document.write(i+"<br/>");
+    i++;
+}
+var j=11;
+do{
+    document.write(j+"<br/>");
+    j++
+}
+while(j <= 15);
+// document.writeln(x === 10.0)
+// document.writeln(x);
+// document.writeln(y);
+// document.writeln(name);
+// document.writeln(sentence);
+// document.writeln(result);
